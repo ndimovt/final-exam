087373785eca5d3ae48b8857aa73d87ac71d42fb
ndimovt <nikolaitodorov119@gmail.com>
2024-09-07 13:54:26 +0300

    Added constructors, getters and setters in Team.java Created dto package in model package Created LongestPlayingPairDto.java Added spring boot validation dependency in pom.xml Added annotations and validations in Player.java

--- src/main/java/com/example/demo/model/Player.java
+++ src/main/java/com/example/demo/model/Player.java
@@ -1,7 +1,27 @@
 package com.example.demo.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.*;
 
 @Entity
 public class Player {
+    @Id
+    @NotNull
+    private Long id;
+    @NotNull
+    @Min(1)
+    @Max(99)
+    private int teamNumber;
+    @NotNull
+    @Size(min = 2, max = 2)
+    @Pattern(regexp = "^[A-Z]{2}$", message = "Input must have 2 uppercase symbols!")
+    private String position;
+    @NotNull
+    @Pattern(regexp = "^([^\\W\\d_]+-?){1,7}$", message = "Username can't contain special symbols or numbers!")
+    private String name;
+    @ManyToOne
+    private Team teamId;
+
 }
--- src/main/java/com/example/demo/model/Team.java
+++ src/main/java/com/example/demo/model/Team.java
@@ -1,8 +1,46 @@
 package com.example.demo.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Team {
+    @Id
     private Long id;
     private String name;
     private String managerName;
     private char group;
+
+    public Team() {
+    }
+
+    public Team(String name, String managerName, char group) {
+        this.name = name;
+        this.managerName = managerName;
+        this.group = group;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getManagerName() {
+        return managerName;
+    }
+
+    public void setManagerName(String managerName) {
+        this.managerName = managerName;
+    }
+
+    public char getGroup() {
+        return group;
+    }
+
+    public void setGroup(char group) {
+        this.group = group;
+    }
 }

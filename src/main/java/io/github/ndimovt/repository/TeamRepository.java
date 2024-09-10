8c429e4df5e15d28d2e23a9caeb39636e2f8287d
ndimovt <nikolaitodorov119@gmail.com>
2024-09-10 12:19:13 +0300

    added javadoc to TeamRepository.java

--- src/main/java/io/github/ndimovt/repository/TeamRepository.java
+++ src/main/java/io/github/ndimovt/repository/TeamRepository.java
@@ -4,6 +4,9 @@ import io.github.ndimovt.model.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * The interface TeamRepository
+ */
 @Repository
-public interface TeamRepository extends JpaRepository<Team, Long> {
+public interface TeamRepository extends JpaRepository<Team, Long>{
 }
